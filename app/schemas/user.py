2cbab556f7c3ca8b4597bc33745b41efa425cde3
portlexs <portnyagin924@gmail.com>
2025-09-04 19:59:55 +0600

    feat: add model_config in UserRead, delete docstrings in schemas/user

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,33 +1,26 @@
 import uuid
 from typing import Optional
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class UserRead(BaseModel):
-    """User read schema"""
+    model_config = ConfigDict(from_attributes=True)
 
     id: uuid.UUID
-    email: EmailStr
 
 
 class UserCreate(BaseModel):
-    """User create schema"""
-
     email: EmailStr
     password: str
 
 
 class UserLogin(BaseModel):
-    """User login schema"""
-
     email: EmailStr
     password: str
 
 
 class UserUpdate(BaseModel):
-    """User update schema"""
-
     email: Optional[EmailStr] = None
     password: Optional[str] = None
 
