2905f51010b165360ee84fd18bb1f9c17191e98c
portlexs <portnyagin924@gmail.com>
2025-09-04 20:44:49 +0600

    feat: add email in UserRead schema and add model_validators in users endpoinds

--- app/routes/users.py
+++ app/routes/users.py
@@ -7,16 +7,20 @@ from core.jwt import TokenType, create_token
 from core.security import verify_password
 from db.dependencies import get_db
 from services.user import create_user, get_user_by_email, get_user_by_id
-from schemas.user import UserCreate, UserLogin, UserLoginResponse
+from schemas.user import UserCreate, UserLogin, UserLoginResponse, UserRead
 
 
 router = APIRouter(prefix="/users", tags=["users"])
 
 
 @router.get("/{user_id}")
-async def get_user(user_id: uuid.UUID, db: Session = Depends(get_db)):
+async def get_user(user_id: uuid.UUID, db: Session = Depends(get_db)) -> UserRead:
     """Get user in blog by id"""
-    return get_user_by_id(db, user_id)
+    user = get_user_by_id(db, user_id)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return UserRead.model_validate(user)
 
 
 @router.post("/")
@@ -26,7 +30,7 @@ async def register_user(user_in: UserCreate, db: Session = Depends(get_db)):
         raise HTTPException(status_code=400, detail="Email already registered")
 
     user = create_user(db, user_in)
-    return user
+    return UserRead.model_validate(user)
 
 
 @router.post("/login")
@@ -38,7 +42,8 @@ async def login_user(
     if not user or not verify_password(user_in.password, user.password):
         raise HTTPException(status_code=401, detail="Invalid email or password")
 
-    token = create_token(TokenType.ACCESS, {"id": str(user.id), "email": user.email})
+    user_data = UserRead.model_validate(user)
+    token = create_token(TokenType.ACCESS, user_data.model_dump())
     return UserLoginResponse(access_token=token)
 
 
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -8,6 +8,7 @@ class UserRead(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: uuid.UUID
+    email: EmailStr
 
 
 class UserCreate(BaseModel):
