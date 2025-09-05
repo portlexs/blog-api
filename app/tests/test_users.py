84900170edff071e7f3af4e45dec12e044afa1a9
portlexs <portnyagin924@gmail.com>
2025-09-05 12:18:18 +0600

    refactor: rename test funcs and class for register_user

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -2,24 +2,21 @@ from fastapi import status
 from fastapi.testclient import TestClient
 
 
-class TestUserRegister:
+class TestRegisterUser:
 
-    def test_register_user_success(self, client: TestClient) -> None:
+    def test_user_registered_in_db(self, client: TestClient) -> None:
         user_data = {"email": "test_user@example.com", "password": "password123"}
 
         response = client.post("/api/users/", json=user_data)
-
-        assert response.status_code == status.HTTP_200_OK
-
         response_data = response.json()
 
+        assert response.status_code == status.HTTP_200_OK
         assert "id" in response_data
         assert "email" in response_data
         assert response_data["email"] == user_data["email"]
-
         assert "password" not in response_data
 
-    def test_register_user_email_exists(self, client: TestClient) -> None:
+    def test_email_exists(self, client: TestClient) -> None:
         user_data = {"email": "test_user@example.com", "password": "password123"}
 
         client.post("/api/users/", json=user_data)
@@ -29,14 +26,21 @@ class TestUserRegister:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.json() == {"detail": "Email already registered"}
 
-    def test_register_user_invalid_email(self, client: TestClient) -> None:
+    def test_invalid_email(self, client: TestClient) -> None:
         user_data = {"email": "invalid_email", "password": "password123"}
 
         response = client.post("/api/users/", json=user_data)
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
 
-    def test_register_user_empty_password(self, client: TestClient) -> None:
+    def test_empty_email(self, client: TestClient) -> None:
+        user_data = {"email": "", "password": "password123"}
+
+        response = client.post("/api/users/", json=user_data)
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+    def test_empty_password(self, client: TestClient) -> None:
         user_data = {"email": "test_user@example.com", "password": ""}
 
         response = client.post("/api/users/", json=user_data)
