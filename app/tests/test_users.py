ba1a1a440bede3f346ebfab35a1849a5e52fa9a3
portlexs <portnyagin924@gmail.com>
2025-09-05 13:06:14 +0600

    feat(test): add tests for get user endpoint

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -1,3 +1,5 @@
+import uuid
+
 from fastapi import status
 from fastapi.testclient import TestClient
 
@@ -89,5 +91,31 @@ class TestLoginUser:
         user_data = {"email": "test_user@example.com", "password": ""}
 
         response = client.post("/api/users/login", json=user_data)
+
+        assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+class TestGetUser:
+
+    def test_successful_create_and_get_user(self, client: TestClient) -> None:
+        user_data = {"email": "test_user@example.com", "password": "password123"}
+
+        response = client.post("/api/users/", json=user_data)
+
+        response = client.get(f"/api/users/{response.json()['id']}")
+
+        assert response.status_code == status.HTTP_200_OK
+        assert "id" in response.json()
+        assert "email" in response.json()
+        assert response.json()["email"] == user_data["email"]
+        assert "password" not in response.json()
+
+    def test_user_not_found(self, client: TestClient) -> None:
+        response = client.get(f"/api/users/{uuid.uuid4()}")
+
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_invalid_uuid(self, client: TestClient) -> None:
+        response = client.get("/api/users/invalid_uuid")
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
