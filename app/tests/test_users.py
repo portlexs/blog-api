f07859b48e333198ef968f9439470597a3bcd6de
portlexs <portnyagin924@gmail.com>
2025-09-05 13:47:16 +0600

    feat(tests): add update user tests

--- app/tests/test_users.py
+++ app/tests/test_users.py
@@ -119,3 +119,92 @@ class TestGetUser:
         response = client.get("/api/users/invalid_uuid")
 
         assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+
+
+class TestUpdateUser:
+
+    def test_successful_update_user_email(self, client: TestClient) -> None:
+        # create user and get his id
+        user_data = {"email": "test_user@example.com", "password": "password123"}
+        create_response = client.post("/api/users/", json=user_data)
+        user_id = create_response.json()["id"]
+
+        # update user email
+        user_data_to_update = {"email": "new_test_user@example.com"}
+        update_response = client.put(f"/api/users/{user_id}", json=user_data_to_update)
+
+        assert update_response.status_code == status.HTTP_200_OK
+
+        # login user with new email
+        new_user_data = {
+            "email": "new_test_user@example.com",
+            "password": "password123",
+        }
+        login_response = client.post("/api/users/login", json=new_user_data)
+
+        assert login_response.status_code == status.HTTP_200_OK
+
+    def test_successful_update_user_password(self, client: TestClient) -> None:
+        # create user and get his id
+        user_data = {"email": "test_user@example.com", "password": "password123"}
+        create_user_response = client.post("/api/users/", json=user_data)
+        user_id = create_user_response.json()["id"]
+
+        # update user password
+        user_data_to_update = {"password": "new_password123"}
+        update_response = client.put(f"/api/users/{user_id}", json=user_data_to_update)
+
+        assert update_response.status_code == status.HTTP_200_OK
+        assert "password" not in update_response.json()
+
+        # login user with new password
+        new_user_data = {
+            "email": "test_user@example.com",
+            "password": "new_password123",
+        }
+        login_response = client.post("/api/users/login", json=new_user_data)
+
+        assert login_response.status_code == status.HTTP_200_OK
+        assert "access_token" in login_response.json()
+
+    def test_successful_update_user_email_and_password(
+        self, client: TestClient
+    ) -> None:
+        # create user and get his id
+        user_data = {"email": "test_user@example.com", "password": "password123"}
+        create_response = client.post("/api/users/", json=user_data)
+        user_id = create_response.json()["id"]
+
+        # update user email and password
+        user_data_to_update = {
+            "email": "new_test_user@example.com",
+            "password": "new_password123",
+        }
+        update_response = client.put(f"/api/users/{user_id}", json=user_data_to_update)
+
+        assert update_response.status_code == status.HTTP_200_OK
+        assert "password" not in update_response.json()
+
+        # login user with new email and password
+        login_response = client.post("/api/users/login", json=user_data_to_update)
+
+        assert login_response.status_code == status.HTTP_200_OK
+
+    def test_updating_user_not_found(self, client: TestClient) -> None:
+        user_data = {"email": "test_user@example.com", "password": "password123"}
+
+        update_response = client.put(f"/api/users/{uuid.uuid4()}", json=user_data)
+
+        assert update_response.status_code == status.HTTP_404_NOT_FOUND
+
+    def test_updating_email_already_exists(self, client: TestClient) -> None:
+        # create user and get his id
+        user_data = {"email": "test_user@example.com", "password": "password123"}
+        create_response = client.post("/api/users/", json=user_data)
+        user_id = create_response.json()["id"]
+
+        # try to update user email
+        user_data_to_update = {"email": "test_user@example.com"}
+        update_response = client.put(f"/api/users/{user_id}", json=user_data_to_update)
+
+        assert update_response.status_code == status.HTTP_400_BAD_REQUEST
