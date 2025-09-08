d1edc48421d122fe67b1a53de4e01adcae558bcd
portlexs <portnyagin924@gmail.com>
2025-09-08 19:31:34 +0600

    refactor(scheme): rename article scheme

--- app/schemas/articles.py
+++ app/schemas/articles.py
@@ -5,14 +5,21 @@ from typing import Optional, List
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class BaseArticle(BaseModel):
+class ArticleCreate(BaseModel):
     title: str = Field(..., min_length=3, max_length=100)
     description: str
     body: str
     tag_list: Optional[List[str]]
 
 
-class ArticleInfoResponse(BaseArticle):
+class ArticleUpdate(BaseModel):
+    title: Optional[str] = Field(None, min_length=3, max_length=50)
+    description: Optional[str]
+    body: Optional[str]
+    tag_list: Optional[List[str]]
+
+
+class ArticleInfoResponse(ArticleCreate):
     model_config = ConfigDict(from_attributes=True)
 
     id: uuid.UUID
@@ -23,15 +30,3 @@ class ArticleInfoResponse(BaseArticle):
 
 class AllArticlesResponse(BaseModel):
     articles: List[ArticleInfoResponse]
-    articles_count: int
-
-
-class CreateArticleRequest(BaseArticle):
-    pass
-
-
-class UpdateArticleRequest(BaseModel):
-    title: Optional[str] = Field(None, min_length=3, max_length=50)
-    description: Optional[str]
-    body: Optional[str]
-    tag_list: Optional[List[str]]
