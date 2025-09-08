2b13ee47abb748fe487ef9b35a8be8c819570402
portlexs <portnyagin924@gmail.com>
2025-09-08 19:33:02 +0600

    feat(model): add user relationship, nellable=False and title validate

--- app/models/articles.py
+++ app/models/articles.py
@@ -1,8 +1,9 @@
 import uuid
 from datetime import datetime
 
-from sqlalchemy import DateTime, String, func
-from sqlalchemy.orm import Mapped, mapped_column
+from slugify import slugify
+from sqlalchemy import DateTime, ForeignKey, String, func
+from sqlalchemy.orm import Mapped, Relationship, mapped_column, validates
 from sqlalchemy.dialects.postgresql import ARRAY, UUID
 
 from db.session import Base
@@ -14,10 +15,18 @@ class Article(Base):
     id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
     )
-    slug: Mapped[str] = mapped_column(String(50), unique=True)
-    title: Mapped[str] = mapped_column(String(100))
-    description: Mapped[str]
-    body: Mapped[str]
-    tag_list: Mapped[list[str]] = mapped_column(ARRAY(String), default=[])
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), ForeignKey("users.id"), nullable=False
+    )
+    slug: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(nullable=False)
+    body: Mapped[str] = mapped_column(nullable=False)
+    tag_list: Mapped[list[str]] = mapped_column(ARRAY(String), default=list)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     is_deleted: Mapped[bool] = mapped_column(default=False)
+
+    @validates("title")
+    def validate_title(self, key, title):
+        self.slug = slugify(title)
+        return title
