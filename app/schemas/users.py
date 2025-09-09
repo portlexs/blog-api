2a994f8bc39f6424936f294e44bff2612dd5632b
portlexs <portnyagin924@gmail.com>
2025-09-09 18:44:27 +0600

    refactor(scheme): replace update user scheme in file

--- app/schemas/users.py
+++ app/schemas/users.py
@@ -16,6 +16,14 @@ class UserCreate(UserLogin):
     image_url: Optional[AnyUrl] = Field(None, max_length=2048)
 
 
+class UserUpdate(BaseModel):
+    email: Optional[EmailStr] = Field(None)
+    username: Optional[str] = Field(None, min_length=3, max_length=50)
+    password: Optional[str] = Field(None, min_length=6)
+    bio: Optional[str] = Field(None, max_length=255)
+    image_url: Optional[str] = Field(None, max_length=2048)
+
+
 class UserInfoResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
@@ -26,14 +34,6 @@ class UserInfoResponse(BaseModel):
     image_url: Optional[AnyUrl] = None
     created_at: datetime
     is_banned: bool
-
-
-class UserUpdate(BaseModel):
-    email: Optional[EmailStr] = Field(None)
-    username: Optional[str] = Field(None, min_length=3, max_length=50)
-    password: Optional[str] = Field(None, min_length=6)
-    bio: Optional[str] = Field(None, max_length=255)
-    image_url: Optional[str] = Field(None, max_length=2048)
 
 
 class UserLoginResponse(BaseModel):
