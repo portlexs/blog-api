82c884915182d39a246a294c83428862294c8848
portlexs <portnyagin924@gmail.com>
2025-09-14 12:57:28 +0600

    feat(route): add health check in api router

--- app/routes/api_router.py
+++ app/routes/api_router.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
+from typing import Dict
+
+from fastapi import APIRouter, status
 
 from routes import articles, comments, users
 
 
 api_router = APIRouter(prefix="/api")
+
+
+@api_router.get("/health", status_code=status.HTTP_200_OK)
+async def health() -> Dict[str, str]:
+    return {"status": "ok"}
+
 
 api_router.include_router(users.router)
 api_router.include_router(articles.router)
