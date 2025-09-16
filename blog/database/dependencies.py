8b78d9a4f6021f884fe44ec3909ab7924e70023c
portlexs <portnyagin924@gmail.com>
2025-09-16 17:31:42 +0600

    refactor(database): rename variable in get_db function for clarity

--- blog/database/dependencies.py
+++ blog/database/dependencies.py
@@ -6,5 +6,5 @@ from database.session import SessionLocal
 
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
-    async with SessionLocal() as db:
-        yield db
+    async with SessionLocal() as session:
+        yield session
