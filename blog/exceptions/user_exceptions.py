78a6d9a638f8cd41571c78d0c176494a66e5b0aa
portlexs <portnyagin924@gmail.com>
2025-09-16 21:10:51 +0600

    fix(exceptions): simplify UserAlreadyExists exception by removing unused entity parameter

--- blog/exceptions/user_exceptions.py
+++ blog/exceptions/user_exceptions.py
@@ -2,8 +2,8 @@ from fastapi import HTTPException, status
 
 
 class UserAlreadyExists(HTTPException):
-    def __init__(self, entity: str, field: str, value: str) -> None:
+    def __init__(self, field: str, value: str) -> None:
         super().__init__(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"{entity} with {field}={value} already exists",
+            detail=f"User with {field} '{value}' already exists",
         )
