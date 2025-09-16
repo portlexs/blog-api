32f076a67185288f6f115660ee2780ee966f194f
portlexs <portnyagin924@gmail.com>
2025-09-16 21:03:43 +0600

    refactor(dependencies): rename get_db to get_session and add new SessionDep type

--- blog/database/dependencies.py
+++ blog/database/dependencies.py
@@ -1,10 +1,14 @@
-from typing import AsyncGenerator
+from typing import Annotated, AsyncGenerator
 
+from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database.session import SessionLocal
 
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
     async with SessionLocal() as session:
         yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
