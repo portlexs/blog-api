aada98d3fd259e39bdf1ed3e01a7284b5ec0a594
portlexs <portnyagin924@gmail.com>
2025-09-20 11:33:21 +0600

    refactor(user-repository): split get_user method on get by id, username, usermane_or_email

--- blog/repositories/user_repository.py
+++ blog/repositories/user_repository.py
@@ -1,6 +1,7 @@
+import uuid
 from typing import Any, Dict, Optional
 
-from sqlalchemy import select
+from sqlalchemy import and_, or_, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models.user_model import User
@@ -10,11 +11,6 @@ class UserRepository:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def get_user(self, **filters) -> Optional[User]:
-        query = select(User).filter_by(**filters)
-        result = await self.session.execute(query)
-        return result.scalars().one_or_none()
-
     async def create_user(self, user_data: Dict[str, Any]) -> User:
         db_user = User(**user_data)
 
@@ -23,3 +19,20 @@ class UserRepository:
         await self.session.refresh(db_user)
 
         return db_user
+
+    async def get_user_by_id(self, user_id: uuid.UUID) -> Optional[User]:
+        query = select(User).where(User.id == user_id)
+        result = await self.session.execute(query)
+        return result.scalars().one_or_none()
+
+    async def get_user_by_username(self, username: str) -> Optional[User]:
+        query = select(User).where(User.username == username)
+        result = await self.session.execute(query)
+        return result.scalars().one_or_none()
+
+    async def get_user_by_email_or_username(
+        self, email: str, username: str
+    ) -> Optional[User]:
+        query = select(User).where(or_(User.email == email, User.username == username))
+        result = await self.session.execute(query)
+        return result.scalars().one_or_none()
