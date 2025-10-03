70eda02141978a310e1a5ac73e9a6f482ebdbe47
portlexs <portnyagin924@gmail.com>
2025-10-04 00:30:52 +0600

    fix(tests): delete test user register with full_data, invalid avatar_url. Add test register with existing email and username

--- app/tests/test_user/test_register_user.py
+++ app/tests/test_user/test_register_user.py
@@ -7,24 +7,7 @@ from app.tests.clients.user_client import UserClient
 
 @pytest.mark.asyncio
 class TestRegisterUser:
-    async def test_successful_register_with_full_data(self, user_client: UserClient):
-        register_response = await user_client.register_user(
-            {
-                "username": "test",
-                "email": "email@example.com",
-                "password": "password",
-                "biography": "test",
-                "avatar_url": "https://example.com/avatar.jpg",
-            }
-        )
-
-        assert register_response.status_code == HTTPStatus.CREATED
-        assert "access_token" in register_response.json()
-        assert "refresh_token" in register_response.json()
-
-    async def test_successful_register_with_required_data(
-        self, user_client: UserClient
-    ):
+    async def test_successful_register(self, user_client: UserClient):
         register_response = await user_client.register_user(
             {"username": "test", "email": "email@example.com", "password": "password"}
         )
@@ -59,21 +42,38 @@ class TestRegisterUser:
 
         assert register_response.status_code == HTTPStatus.BAD_REQUEST
 
+    async def test_register_user_with_existing_username_and_email(
+        self, user_client: UserClient
+    ):
+        register_response1 = await user_client.register_user(
+            {
+                "username": "test1",
+                "email": "email1@example.com",
+                "password": "password1",
+            }
+        )
+
+        register_response2 = await user_client.register_user(
+            {
+                "username": "test2",
+                "email": "email2@example.com",
+                "password": "password1",
+            }
+        )
+
+        register_response = await user_client.register_user(
+            {
+                "username": "test1",
+                "email": "email2@example.com",
+                "password": "password2",
+            }
+        )
+
+        assert register_response.status_code == HTTPStatus.BAD_REQUEST
+
     async def test_register_user_with_invalid_email(self, user_client: UserClient):
         register_response = await user_client.register_user(
             {"username": "test", "email": "invalid_email", "password": "password"}
-        )
-
-        assert register_response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
-
-    async def test_register_user_with_invalid_avatar_url(self, user_client: UserClient):
-        register_response = await user_client.register_user(
-            {
-                "username": "test",
-                "email": "email@example.com",
-                "password": "password",
-                "avatar_url": "invalid_url",
-            }
         )
 
         assert register_response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
