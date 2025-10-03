77f0656f4e7ff27c30e49ebf17f3a9db989fa326
portlexs <portnyagin924@gmail.com>
2025-10-03 11:06:11 +0600

    refactor: replace api router from app module to __init__ in routers dir

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
 import uvicorn
+from fastapi import FastAPI
 
-from app.config import settings
+# from .config import settings
+from .routers import api_router
+
+
+app = FastAPI(title="Blog API")
+app.include_router(api_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="app:app", host=settings.api.host, port=settings.api.port)
+    uvicorn.run(app=app)
