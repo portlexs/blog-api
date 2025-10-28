5ca6fe3e70a9088712c522bc7aa3acaabff38ad2
portlexs <portnyagin924@gmail.com>
2025-10-28 18:20:00 +0600

    fix: add ArticleAlreadyExistsError and rename some status codes in comment and user exceptions

--- app/exceptions/article_exceptions.py
+++ app/exceptions/article_exceptions.py
@@ -7,3 +7,11 @@ class ArticleNotFoundError(HTTPException):
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Article not found",
         )
+
+
+class ArticleAlreadyExistsError(HTTPException):
+    def __init__(self) -> None:
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Article already exists",
+        )
--- app/exceptions/user_exceptions.py
+++ app/exceptions/user_exceptions.py
@@ -4,7 +4,7 @@ from fastapi import HTTPException, status
 class UserAlreadyExistsError(HTTPException):
     def __init__(self) -> None:
         super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
+            status_code=status.HTTP_409_CONFLICT,
             detail="User already exists",
         )
 
@@ -28,6 +28,6 @@ class InvalidLoginOrPasswordError(HTTPException):
 class UserNotActiveError(HTTPException):
     def __init__(self) -> None:
         super().__init__(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail="User is not active",
         )
