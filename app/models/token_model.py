fef9daa37985b656ff0fff598c4ec6ef2417f922
portlexs <portnyagin924@gmail.com>
2025-10-28 18:20:34 +0600

    fix: change Base import in models

--- app/models/token_model.py
+++ app/models/token_model.py
@@ -5,7 +5,7 @@ from sqlalchemy import DateTime, ForeignKey, func
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
-from ..database.session import Base
+from ..database.base import Base
 
 
 class Token(Base):
