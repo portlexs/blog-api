e6e7bdc0ddfebf4757ecf00dc612ccd34d7fb712
portlexs <portnyagin924@gmail.com>
2025-10-28 18:23:54 +0600

    feat: add update comment router

--- app/routers/comments_router.py
+++ app/routers/comments_router.py
@@ -4,8 +4,9 @@ from fastapi import APIRouter, status
 
 from ..schemas.comment_schemas import (
     CommentCreate,
-    PublicComment,
+    CommentUpdate,
     GetCommentsResponse,
+    PublicComment,
 )
 from ..services.dependencies import CurrentUserDep, CommentServiceDep
 
@@ -18,9 +19,8 @@ router = APIRouter(prefix="/articles/{article_slug}/comments", tags=["comments"]
     response_model=GetCommentsResponse,
     status_code=status.HTTP_200_OK,
 )
-async def get_comments(
+async def get_article_comments(
     article_slug: str,
-    _current_user: CurrentUserDep,
     comment_service: CommentServiceDep,
 ) -> GetCommentsResponse:
     comments = await comment_service.get_article_comments(article_slug)
@@ -44,11 +44,29 @@ async def create_comment(
     return PublicComment.model_validate(comment)
 
 
+@router.post(
+    "/{comment_id}",
+    response_model=PublicComment,
+    status_code=status.HTTP_200_OK,
+)
+async def update_comment(
+    article_slug: str,
+    comment_id: uuid.UUID,
+    comment_in: CommentUpdate,
+    current_user: CurrentUserDep,
+    comment_service: CommentServiceDep,
+) -> PublicComment:
+    comment = await comment_service.update_comment(
+        article_slug, comment_id, comment_in, current_user.id
+    )
+    return PublicComment.model_validate(comment)
+
+
 @router.delete("/{comment_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_comment(
     article_slug: str,
     comment_id: uuid.UUID,
-    _current_user: CurrentUserDep,
+    current_user: CurrentUserDep,
     comment_service: CommentServiceDep,
 ) -> None:
-    await comment_service.delete_comment(article_slug, comment_id)
+    await comment_service.delete_comment(article_slug, comment_id, current_user.id)
