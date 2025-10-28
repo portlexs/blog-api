67980e9bbafb21829e6b1a5f10ae9e75006bc014
portlexs <portnyagin924@gmail.com>
2025-10-28 18:22:14 +0600

    feat & fix: add update comment method and add user_id in get_comment method

--- app/repositories/comment_repository.py
+++ app/repositories/comment_repository.py
@@ -5,6 +5,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from ..models.comment_model import Comment
+from ..schemas.comment_schemas import CommentUpdate
 
 
 class CommentRepository:
@@ -17,16 +18,31 @@ class CommentRepository:
         return result.scalars().all()
 
     async def get_comment(
-        self, article_id: uuid.UUID, comment_id: uuid.UUID
+        self, article_id: uuid.UUID, comment_id: uuid.UUID, user_id: uuid.UUID
     ) -> Optional[Comment]:
         query = select(Comment).where(
-            Comment.article_id == article_id, Comment.id == comment_id
+            Comment.article_id == article_id,
+            Comment.id == comment_id,
+            Comment.user_id == user_id,
         )
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
 
     async def create_comment(self, comment: Comment) -> Comment:
         self.session.add(comment)
+        await self.session.commit()
+        await self.session.refresh(comment)
+
+        return comment
+
+    async def update_comment(
+        self, comment: Comment, comment_in: CommentUpdate
+    ) -> Comment:
+        comment_data = comment_in.model_dump(mode="json")
+
+        for key, value in comment_data.items():
+            setattr(comment, key, value)
+
         await self.session.commit()
         await self.session.refresh(comment)
 
