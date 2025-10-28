0fc1abeaf5fb8397264f37de6d2a35d61154c3c5
portlexs <portnyagin924@gmail.com>
2025-10-28 18:28:46 +0600

    feat(tests): update article client and add some article tests

--- app/tests/clients/artcles_client.py
+++ app/tests/clients/artcles_client.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict
 
 from httpx import AsyncClient, Response
 
@@ -8,30 +8,30 @@ class ArticleClient:
         self.client = client
         self.base_url = "/api/articles"
 
-    async def get_user_articles(self, headers: Optional[Dict]) -> Response:
+    async def get_all_user_articles(self, headers: Dict | None = None) -> Response:
         return await self.client.get(f"{self.base_url}/", headers=headers)
 
-    async def get_user_article(
-        self, article_slug: int, headers: Optional[Dict]
+    async def get_article(
+        self, article_slug: str, headers: Dict | None = None
     ) -> Response:
         return await self.client.get(f"{self.base_url}/{article_slug}", headers=headers)
 
     async def create_article(
-        self, article_in: Dict, headers: Optional[Dict]
+        self, article_in: Dict, headers: Dict | None = None
     ) -> Response:
         return await self.client.post(
             f"{self.base_url}/", json=article_in, headers=headers
         )
 
     async def update_article(
-        self, article_slug: str, article_in: Dict, headers: Optional[Dict]
+        self, article_slug: str, article_in: Dict, headers: Dict | None = None
     ) -> Response:
         return await self.client.put(
             f"{self.base_url}/{article_slug}", json=article_in, headers=headers
         )
 
     async def delete_article(
-        self, article_slug: str, headers: Optional[Dict]
+        self, article_slug: str, headers: Dict | None = None
     ) -> Response:
         return await self.client.delete(
             f"{self.base_url}/{article_slug}", headers=headers
